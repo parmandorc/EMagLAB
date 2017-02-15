c8a824655e358383811f7a814d3547fe60698ffe
Pablo Armando Rodríguez Codes <parmandorc@gmail.com>
2017-02-15 13:48:17 +0100

    Added network awareness to some components
    Added network awareness to a few components, which were having some issues
    with synchronization across the network, causing clients without authority
    to see some object sweep their position (since they were being assigned a
    position by said client, then having it overriden by the correct
    position).

--- Stage2Project/Assets/Scripts/RandomStartPosition.cs
+++ Stage2Project/Assets/Scripts/RandomStartPosition.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class RandomStartPosition : MonoBehaviour
+public class RandomStartPosition : NetworkBehaviour
 {
-	void Start()
+    /* This should only execute on the Server's Start, since if not it will cause several weird behaviours.
+     * If it runs on the client, whenever said client connects to the game, all objects with this component will
+     *  calculate a random position, which will then be overridden by the server's position, causing all the objects
+     *  in the client's screen to sweep to their final position.
+     * If it runs on the authority, the previous problem is solved. However, whenever a player joins the game,
+     *  the host (server) sees the player for a moment at (0,0), and then the player's objects is swept to their final
+     *  position (after the new client's Start is executed and their random position calculated and transmitted across the network).
+     */ 
+	public override void OnStartServer()
     {
         float hw = Arena.Width * 0.5f;
         float hh = Arena.Height * 0.5f;
