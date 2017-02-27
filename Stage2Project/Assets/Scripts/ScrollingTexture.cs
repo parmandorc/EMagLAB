035210e7e2ce553ecb7d4088aa237bc1f0e4b790
Pablo Armando Rodríguez Codes <parmandorc@gmail.com>
2017-02-27 14:20:20 +0100

    Added final textures for portals

---
+++ Stage2Project/Assets/Scripts/ScrollingTexture.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class ScrollingTexture : MonoBehaviour
+{
+    [SerializeField]
+    private float ScrollingSpeed = 1.0f;
+
+    private Renderer mRenderer;
+
+	void Start ()
+    {
+        mRenderer = gameObject.GetComponent<Renderer>();
+        mRenderer.material.mainTextureOffset = new Vector2(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+    }
+	
+	void Update ()
+    {
+        Vector2 currentOffset = mRenderer.material.mainTextureOffset;
+        mRenderer.material.mainTextureOffset = new Vector2(currentOffset.x, currentOffset.y + ScrollingSpeed * Time.deltaTime);
+    }
+}
