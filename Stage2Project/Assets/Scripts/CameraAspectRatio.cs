c95414f60072cd656e39c00928e8c2445764e7b3
Pablo Armando Rodríguez Codes <parmandorc@gmail.com>
2017-02-27 14:56:04 +0100

    Fixed screen aspect ratio to 1:1
    Fixed camera's aspect ratio to square 1:1 for gameplay reasons:
    map is square and it is not wanted that the scene is rendered outside that
    square.

---
+++ Stage2Project/Assets/Scripts/CameraAspectRatio.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Class for fixing the aspect ratio of the screen to 1:1 (for gameplay reasons: map is squared).
+ * For now, since the game doesn't allow resizing of the window, we only need to fix the ratio on Start.
+ */ 
+[RequireComponent(typeof(Camera))]
+public class CameraAspectRatio : MonoBehaviour
+{
+    private Camera mCamera;
+
+    void Start()
+    {
+        mCamera = GetComponent<Camera>();
+
+        FixAspectRatio();
+    }
+
+    void FixAspectRatio()
+    {
+        int width = Screen.width;
+        int height = Screen.height;
+
+        if (width != 0 && height != 0)
+        {
+            float ratio = (float)height / width;
+
+            if (ratio < 1.0f)
+            {
+                mCamera.rect = new Rect((1.0f - ratio) * 0.5f, 0.0f, ratio, 1.0f);
+            }
+            else if (ratio > 1.0f)
+            {
+                mCamera.rect = new Rect(0.0f, (1.0f - 1.0f / ratio) * 0.5f, 1.0f, 1.0f / ratio);
+            }
+
+        }
+    }
+}
