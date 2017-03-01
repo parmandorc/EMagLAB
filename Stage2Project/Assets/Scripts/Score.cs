d5d808345c7dabca270ba39e4b90e424502bc9b6
Pablo Armando Rodríguez Codes <parmandorc@gmail.com>
2017-03-01 17:25:18 +0100

    Added in-game score indicators
    Added indicators above each player with their score during the game.
    This helps with clarity in gameplay.

--- Stage2Project/Assets/Scripts/Score.cs
+++ Stage2Project/Assets/Scripts/Score.cs
@@ -5,8 +5,16 @@ using UnityEngine.Networking;
 
 public class Score : NetworkBehaviour
 {
+    [SerializeField]
+    private Vector3 ScoreOffset;
+
+    [SyncVar]
     private int mScoreOne;
+
+    [SyncVar]
     private int mScoreTwo;
+
+    private TextMesh mInGameScore;
 
     public int TotalScore { get { return mScoreOne + mScoreTwo; } }
 
@@ -14,20 +22,41 @@ public class Score : NetworkBehaviour
     public void DecrementScore(string tag) { UpdateScore(tag, false); }
 
     // The Server must have authority on the Scores, so only the server can update the score values.
-    [ServerCallback]
     private void UpdateScore(string tag, bool doIncrement)
     {
-        int value = doIncrement ? 1 : -1;
-
-        switch (tag)
+        if (isServer)
         {
-            case "ScoreOne":
-                mScoreOne += value;
-                break;
+            int value = doIncrement ? 1 : -1;
 
-            case "ScoreTwo":
-                mScoreTwo += value;
-                break;
+            switch (tag)
+            {
+                case "ScoreOne":
+                    mScoreOne += value;
+                    break;
+
+                case "ScoreTwo":
+                    mScoreTwo += value;
+                    break;
+            }
+
+            RpcUpdateScoreText();
         }
+    }
+
+    void Awake()
+    {
+        mInGameScore = GetComponentInChildren<TextMesh>();
+    }
+
+    void Update()
+    {
+        mInGameScore.transform.rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
+        mInGameScore.transform.position = transform.position + ScoreOffset;
+    }
+
+    [ClientRpc]
+    private void RpcUpdateScoreText()
+    {
+        mInGameScore.text = "Score: " + TotalScore;
     }
 }
